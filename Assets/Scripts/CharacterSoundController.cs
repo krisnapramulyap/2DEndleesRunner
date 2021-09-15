4185bbd8d703bdc02f3f655c7d5d4c7ef0253b35
Krisna Pramulya <19560029@stmik.jayakarta.ac.id>
2021-09-15 20:04:50 +0700

    first commit

---
+++ Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    public AudioClip jump;
+    public AudioClip scoreHighlight;
+    public AudioClip gameOver;
+
+    private AudioSource audioPlayer;
+
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+
+    public void PlayGameOver()
+    {
+        audioPlayer.PlayOneShot(gameOver);
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# 2D-Endless-Runner
+Assignment Chapter 2 Game Progrogramming Studi Independen Agate
+
+File build ada di release
